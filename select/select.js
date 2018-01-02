3b8d4473b0ac5cc0fb8af3bcbabb9d930811bbad
梁彪 <528528219@qq.com>
2018-01-02 09:49:56 +0800

    '1.0'

---
+++ select/select.js
@@ -0,0 +1,21 @@
+new Vue({
+    el:"#app",
+    data:{
+        datas:[
+            {id:1,title:"这是第一个"},
+            {id:2,title:"这是第二个"},
+            {id:3,title:"这是第三个"},
+        ],
+        state:false,
+        title:''
+    },
+    methods:{
+        aa(){
+            this.state=true;
+        },
+        bb(val){
+            this.title=val;
+            this.state=false;
+        }
+    }
+})
